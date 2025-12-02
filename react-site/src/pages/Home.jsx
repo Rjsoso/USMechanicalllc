3c33126de20d020ccbc567ee9df785df452cedae
Rory Bylund <rorybylund@Rorys-MacBook-Pro.local>
2025-12-02 15:35:32 -0700

    Revert NavBar and GlassMenuButton components

--- react-site/src/pages/Home.jsx
+++ react-site/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react'
 import Header from '../components/Header'
-import NavBar from '../components/NavBar'
 import HeroSection from '../components/HeroSection'
 import AboutAndSafety from '../components/AboutAndSafety'
 import CompanyStats from '../components/CompanyStats'
@@ -19,7 +18,6 @@ export default function Home() {
   return (
     <>
       <Header />
-      <NavBar />
 
       <main>
         <section id="hero" style={{ position: 'fixed', top: 0, left: 0, width: '100%', zIndex: 0 }}>
