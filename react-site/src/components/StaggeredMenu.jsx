183823b4f6ac565d14fcb77cecf374c1242adfc6
Rory Bylund <rorybylund@Rorys-MacBook-Pro.local>
2025-12-02 15:16:53 -0700

    Fix useEffect import

--- react-site/src/components/StaggeredMenu.jsx
+++ react-site/src/components/StaggeredMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useLayoutEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 import './StaggeredMenu.css';
 
