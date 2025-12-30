d6a98ec0500e5e2abe89dbeb36314906f36eeef7
Rory Bylund <rorybylund@Rorys-MacBook-Pro.local>
2025-12-30 10:14:40 -0700

    Adjust dock sizing

--- react-site/src/components/Dock.jsx
+++ react-site/src/components/Dock.jsx
@@ -78,9 +78,9 @@ export default function Dock({
   items,
   className = '',
   spring = { mass: 0.1, stiffness: 150, damping: 12 },
-  magnification = 70,
+  magnification = 69,
   distance = 200,
-  panelHeight = 68,
+  panelHeight = 70,
   baseItemSize = 50,
 }) {
   const mouseX = useMotionValue(Infinity);
