02a0fd2743e57bc6c6bceadbe3680c52120f1835
Rory Bylund <rorybylund@Rorys-MacBook-Pro.local>
2025-12-30 19:35:44 -0700

    Update projects nav icon

--- react-site/src/components/Header.jsx
+++ react-site/src/components/Header.jsx
@@ -118,7 +118,7 @@ function Header() {
     ), label: 'Services', onClick: () => scrollToSection('#services') },
     { icon: (
       <svg className="text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24">
-        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m3 16 5-7 6 6.5m6.5 2.5L16 13l-4.286 6M14 10h.01M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z"/>
+        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 4h12M6 4v16M6 4H5m13 0v16m0-16h1m-1 16H6m12 0h1M6 20H5M9 7h1v1H9V7Zm5 0h1v1h-1V7Zm-5 4h1v1H9v-1Zm5 0h1v1h-1v-1Zm-3 4h2a1 1 0 0 1 1 1v4h-4v-4a1 1 0 0 1 1-1Z"/>
       </svg>
     ), label: 'Projects', onClick: () => scrollToSection('#portfolio') },
     { icon: (
