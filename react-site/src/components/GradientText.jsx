edf55a6b7e0f84df6507a5d2ea1e02ebee0b734d
Rory Bylund <rorybylund@Rorys-MacBook-Pro.local>
2026-01-07 10:26:55 -0700

    TEST: Ultra-obvious styling - red text, yellow bg, green border, 2x size

--- react-site/src/components/GradientText.jsx
+++ react-site/src/components/GradientText.jsx
@@ -1,28 +1,15 @@
-import './GradientText.css';
-import { useState, useEffect } from 'react';
-
 export default function GradientText({ children }) {
-  const [color, setColor] = useState('#3404f6');
-  
-  useEffect(() => {
-    console.log('🎨 DIRECT INLINE COLOR ANIMATION on:', children);
-    
-    let hue = 0;
-    const interval = setInterval(() => {
-      // Alternate between blue and red
-      hue = (hue + 1) % 360;
-      const newColor = hue < 180 ? '#3404f6' : '#f40101';
-      setColor(newColor);
-    }, 50);
-    
-    return () => clearInterval(interval);
-  }, [children]);
+  console.log('🔴 TESTING SOLID RED COLOR on:', children);
   
   return (
     <span style={{ 
-      color: color, 
+      color: '#FF0000',
       fontWeight: 900,
-      display: 'inline-block'
+      fontSize: '2em',
+      backgroundColor: 'yellow',
+      padding: '10px',
+      display: 'inline-block',
+      border: '5px solid green'
     }}>
       {children}
     </span>
