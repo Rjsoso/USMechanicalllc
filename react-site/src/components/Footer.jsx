6625b18b81b30ff4a4d396a4f3485bb529d78912
Rory Bylund <rorybylund@gmail.com>
2026-01-26 14:10:07 -0700

    Change footer background to pure black
    Updated footer background from dark navy (#00001a) to pure black
    for cleaner visual appearance.

--- react-site/src/components/Footer.jsx
+++ react-site/src/components/Footer.jsx
@@ -103,7 +103,7 @@ function Footer() {
   }
 
   return (
-    <footer className="bg-[#00001a] text-gray-300">
+    <footer className="bg-black text-gray-300">
       {/* Main Footer Content */}
       <div className="mx-auto max-w-7xl px-6 py-12 lg:px-8">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
